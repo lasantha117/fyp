5cae9a8c02a77c7969b70c47d50e19909e3c7b28
lasantha117 <weerasinghehwlp.20@uom.lk>
2025-07-18 15:34:11 +0530

    added gh files and deleted index.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,15 @@
 // frontend/next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const isProd = process.env.NODE_ENV === 'production';
 
+const nextConfig: NextConfig = {
+  output: "export", // Enables static HTML export
+  basePath: isProd ? '/fyp' : '', // UPDATED: Repository name is 'fyp'
+  assetPrefix: isProd ? '/fyp/' : '', // UPDATED: Repository name is 'fyp'
+  images: {
+    unoptimized: true, // Disable Next.js Image Optimization as it needs a server
+  },
 };
 
 export default nextConfig;
