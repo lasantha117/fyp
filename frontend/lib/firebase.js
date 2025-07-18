cdd31ef0ed2096c43ab44dba2ea4817b08fc19cd
lasantha117 <weerasinghehwlp.20@uom.lk>
2025-07-18 18:56:25 +0530

    firebase.json updated

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -6,17 +6,19 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth"; // Import getAuth
 import { getStorage } from "firebase/storage"; // Import getStorage
 
+
+
 // Your web app's Firebase configuration
-// For security, consider moving these to environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDfp_Ya2eIUMFRg0Ub1ICsgvKqPMoWk4lM", // Replace with your actual API Key
+  apiKey: "AIzaSyDfp_Ya2eIUMFRg0Ub1ICsgvKqPMoWk4lM",
   authDomain: "fyp-bbe4d.firebaseapp.com",
   databaseURL: "https://fyp-bbe4d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "fyp-bbe4d",
-  storageBucket: "fyp-bbe4d.appspot.com", // Your Firebase Storage bucket name
+  storageBucket: "fyp-bbe4d.firebasestorage.app",
   messagingSenderId: "75697914820",
   appId: "1:75697914820:web:7bab142f2d6313cf1c1a91"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig); // Export app itself
